4e44f33c8d324237e649c256a4316432475035aa
dev-rathore <rathoredvd3001@gmail.com>
2022-07-13 11:20:43 +0530

    Updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
+import React from "react";
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <Routes>
+      <Route exact path="/" element={<HomePage/>} />
+      <Route exact path="/shop" element={<ShopPage/>} />
+      {/* <Route exact path="/hats" component={HatsPage} /> */}
+    </Routes>
   );
 }
 
