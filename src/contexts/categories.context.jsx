7413ee3f7055f08fc3057aab4f4abeb2a5c21094
dev-rathore <rathoredvd3001@gmail.com>
2022-07-29 23:35:51 +0530

    Created Categories Components

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -4,12 +4,12 @@ import { createContext, useState, useEffect } from "react";
 import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
 // import SHOP_DATA from '../shop-data.js';
 
-export const ProductsContext = createContext({
-  products: [],
+export const CategoriesContext = createContext({
+  categoriesMap: {},
 });
 
-export const ProductsProvider = ({children}) => {
-  const [products, setProducts] = useState([]);
+export const CategoriesProvider = ({children}) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
 
   // useEffect(() => {
   //   addCollectionAndDocuments('categories', SHOP_DATA);
@@ -18,13 +18,14 @@ export const ProductsProvider = ({children}) => {
   useEffect(() => {
     const getCategoriesMap = async () => {
       const categoryMap = await getCategoriesAndDocuments();
-      console.log(categoryMap);
+      // console.log(categoryMap);
+      setCategoriesMap(categoryMap);
     }
     getCategoriesMap();
   }, []);
 
-  const value = { products };
+  const value = { categoriesMap };
   return (
-    <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
+    <CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider>
   )
 }
